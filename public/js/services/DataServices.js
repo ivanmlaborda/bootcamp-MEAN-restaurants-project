9a5b6b44945147327782045dd140227aaf078f39
devtoni <devtonirm@gmail.com>
2017-08-15 15:22:04 +0200

    modified borough api dataservice

--- public/js/services/DataServices.js
+++ public/js/services/DataServices.js
@@ -1,17 +1,17 @@
 angular.module('Restaurants')
   .factory('DataServices', function ($http) {
     function getAllRestaurants () {
-      var url = `/api/restaurants?limit=null`
+      const url = `/api/restaurants?limit=null`
       return $http.get(url)
     }
 
     function getRestaurantById (id) {
-      var url = '/api/restaurant/' + id
+      const url = '/api/restaurant/' + id
       return $http.get(url)
     }
 
-    function getRestaurantsByBorough (borough, page) {
-      var url = `/api/restaurants/borough/${borough}?&limit=null`
+    function getRestaurantsByBorough (borough) {
+      const url = `/api/restaurants/borough/${borough}?&limit=null`
       return $http.get(url)
     }
 
