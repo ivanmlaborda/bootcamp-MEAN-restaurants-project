29535e69f923b9ec3d124b29553ec729d4330b9e
ivanmlaborda <ivanmlaborda@gmail.com>
2017-08-15 15:20:46 +0200

    cuisine

---
+++ public/js/routes/cuisine/controller.js
@@ -0,0 +1,18 @@
+/* global angular */
+(function () {
+  'use strict'
+
+  function cuisineCtrl ($routeParams, DataServices) {
+    var vm = this
+    var cuisine = $routeParams.cuisine
+    DataServices.getRestaurantsByBorough(cuisine)
+    .then(function (restaurant) {
+      vm.restaurants = restaurant.data
+      console.log(vm.restaurants)
+    })
+  }
+
+  angular
+    .module('Restaurants')
+    .controller('cuisineCtrl', cuisineCtrl)
+})()
