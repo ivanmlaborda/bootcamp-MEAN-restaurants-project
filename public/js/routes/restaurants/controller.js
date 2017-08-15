4e8dba19de3c14d80651bb57577aeb7f48be2e4a
devtoni <devtonirm@gmail.com>
2017-08-15 13:11:22 +0200

    added buttons for pagination

--- public/js/routes/restaurants/controller.js
+++ public/js/routes/restaurants/controller.js
@@ -4,13 +4,17 @@
 
   function restaurantsCtrl (DataServices) {
     var vm = this
-    var page = 1
-    showResults.bind(null, DataServices, vm, page)()
-    vm.page = function () {
-      page++
-      showResults.bind(null, DataServices, vm, page)()
+    vm.page = 1
+    showResults.bind(null, DataServices, vm, vm.page)()
+    vm.pageHandler = function (e) {
+      var valueBtn = e.target.value
+      if (valueBtn === 'Advance') vm.page++
+      else if (valueBtn === 'Pre' && vm.page > 1) vm.page--
+      else vm.page = 1
+      showResults.bind(null, DataServices, vm, vm.page)()
     }
   }
+
   function showResults (DataServices, vm, page) {
     DataServices.getAllRestaurants(page)
     .then(function (restaurants) {
