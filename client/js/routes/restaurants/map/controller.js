73bcf9fa30c73210f9d03766ec588ab784a09a75
ivanmlaborda <ivanmlaborda@gmail.com>
2017-08-18 16:42:22 +0200

    markers ok in map with bounds

--- client/js/routes/restaurants/map/controller.js
+++ client/js/routes/restaurants/map/controller.js
@@ -11,8 +11,14 @@
       zoom: 10
     })
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    //   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    // }).addTo(map)
+
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+      subdomains: 'abcd',
+      maxZoom: 19
     }).addTo(map)
 
     $scope.$on('restaurantsReady', function (e, restaurants) {
@@ -24,7 +30,7 @@
 
       let restaurantData = restaurants.value.data.docs
 
-      var dataGeoJson = GeoJSON.parse(restaurantData, {
+      let dataGeoJson = GeoJSON.parse(restaurantData, {
         Point: ['lat', 'lng'],
         include: ['name', '_id']
       })
@@ -34,6 +40,10 @@
           layer.bindPopup(feature.properties.name)
         }
       }).addTo(map)
+
+      map.flyToBounds(restaurantsGeoJSON.getBounds())
+
+
 
     })
 
